5fb9181a947c043374926f97b1bee2f42e95cba2
IlyaFedorov115 <42892348+IlyaFedorov115@users.noreply.github.com>
2022-10-23 21:27:50 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -6,3 +6,14 @@ SINTER friends:Elena friends:Boris
 
 -- 3
 ZRANK user_r_set Sergey
+
+-- 4
+10,9,8,2,3
+
+HMSET basket:104 banana 3 apple 2 carrot 4 potato 2 tomato 3 cucumber 4
+HKEYS basket:104
+HVALS basket:104
+EXPIRE basket:104 300
+TTL basket:104
+
+-- 5
